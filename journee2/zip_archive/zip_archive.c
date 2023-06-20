6b079b2bbddbad88dcbe47bd151c33d642f21711
Guillaume Lelarge <guillaume@lelarge.info>
2023-06-20 18:38:19 +0200

    Étape 3

--- journee2/zip_archive/zip_archive.c
+++ journee2/zip_archive/zip_archive.c
@@ -2,6 +2,7 @@
 #include "postgres.h"
 #include "fmgr.h"
 #include "utils/builtins.h"
+#include "postmaster/pgarch.h"
 
 /* libzip header */
 #include <zip.h>
@@ -9,15 +10,33 @@
 /* module declaration */
 PG_MODULE_MAGIC;
 
-/* function code */
-
+/* function definitions */
+void        _PG_archive_module_init(ArchiveModuleCallbacks *cb);
+static bool zip_archive_file(const char *file, const char *path);
 PG_FUNCTION_INFO_V1(get_libzip_version);
 
+/* function code */
+
+void
+_PG_archive_module_init(ArchiveModuleCallbacks *cb)
+{
+  cb->archive_file_cb = zip_archive_file;
+}
+
+static bool
+zip_archive_file(const char *file, const char *path)
+{
+  elog(LOG, "file is \"%s\"", file);
+  elog(LOG, "path is \"%s\"", path);
+
+  return true;
+}
+
 /*
- *  * get_libzip_version
- *   *
- *    * Returns libzip version.
- *     */
+ * get_libzip_version
+ *
+ * Returns libzip version.
+ */
 Datum
 get_libzip_version(PG_FUNCTION_ARGS)
 {
