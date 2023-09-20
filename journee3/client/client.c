a2384fc0de3425bbe62132b82b27ca62c467f44c
Guillaume Lelarge <guillaume@lelarge.info>
2023-09-20 08:52:37 +0200

    Squelettes

---
+++ journee3/client/client.c
@@ -0,0 +1,18 @@
+/*
+ * client, testing software
+ *
+ * This software is released under the PostgreSQL Licence.
+ *
+ * Guillaume Lelarge, guillaume@lelarge.info, 2023.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+  printf("ca marche ?\n");
+
+  return 0;
+}
