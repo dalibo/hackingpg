8f0b9b6bf7cf91a74714fedb785b7d0d238b0795
Guillaume Lelarge <guillaume@lelarge.info>
2023-09-20 09:28:15 +0200

    Demande du mot de passe si besoin

--- journee3/client/client.c
+++ journee3/client/client.c
@@ -7,29 +7,55 @@
  *
  */
 
-// #include système
-#include <stdio.h>
-
-// #include PostgreSQL
+// #include
 #include "libpq-fe.h"
+#include "postgres_fe.h"
+#include "common/string.h"
 
 int
 main(int argc, char **argv)
 {
-  const char *conninfo;
-  PGconn     *conn;
+  char   *conninfo;
+  PGconn *conn;
+  char   *password = NULL;
+  bool    new_password;
 
-  if (argc > 1)
-    conninfo = argv[1];
-  else
-    conninfo = "";
-
-  conn = PQconnectdb(conninfo);
-
-  if (PQstatus(conn) != CONNECTION_OK)
+  do
   {
-    fprintf(stderr, "%s", PQerrorMessage(conn));
+    if (argc > 1)
+      conninfo = argv[1];
+    else
+      conninfo = "";
+
+    if (password)
+      sprintf(conninfo, "%s password=%s", conninfo, password);
+
+    new_password = false;
+    conn = PQconnectdb(conninfo);
+
+    if (!conn)
+    {
+      fprintf(stderr, "could not connect\n");
+      return 1;
+    }
+
+    if (PQstatus(conn) == CONNECTION_BAD &&
+        PQconnectionNeedsPassword(conn)  &&
+        !password)
+    {
+      PQfinish(conn);
+      password = simple_prompt("Password: ", false);
+      new_password = true;
+    }
+  } while (new_password);
+
+  if (PQstatus(conn) == CONNECTION_BAD)
+  {
+      fprintf(stderr, "could not connect: %s\n", PQerrorMessage(conn));
+      return 2;
   }
+
+  fprintf(stderr, "Connection successfull!\n");
 
   PQfinish(conn);
 
