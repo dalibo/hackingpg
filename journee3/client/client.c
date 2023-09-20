03e7c83f0cce7cef0725de6d058614abbcd266ac
Guillaume Lelarge <guillaume@lelarge.info>
2023-09-20 12:21:20 +0200

    Mode asynchrone et récupération ligne par ligne

--- journee3/client/client.c
+++ journee3/client/client.c
@@ -22,12 +22,7 @@ main(int argc, char **argv)
   bool      new_password;
   char     *query;
   PGresult *res;
-
-  int       nfields;
-  int       nrows;
-  int       i, j, l;
-  int      *length;
-  char     *pad;
+  int       res_async;
 
   pg_logging_init(argv[0]);
   pg_logging_set_level(PG_LOG_DEBUG);
@@ -77,72 +72,31 @@ main(int argc, char **argv)
   // Trying to execute query
   while (true)
   {
-    res = PQexec(conn, query);
+    res_async = PQsendQuery(conn, query);
 
-    if (PQresultStatus(res) != PGRES_TUPLES_OK)
+    if (!res_async)
     {
-      if (PQstatus(conn) != CONNECTION_OK)
-      {
-        pg_log_info("resetting connection\n");
-        PQreset(conn);
-        if (PQstatus(conn) != CONNECTION_BAD)
-          pg_log_debug("Reset successfull! (backend PID is %d)", PQbackendPID(conn));
-      }
-      else
-      {
-        pg_log_error("query failed: %s", PQerrorMessage(conn));
-      }
+      pg_log_error("query failed: %s", PQerrorMessage(conn));
     }
-    else
+
+    res_async = PQsetSingleRowMode(conn);
+    pg_log_debug("single mode %sactivated", res_async ? "" : "not ");
+
+    while ((res = PQgetResult(conn)))
     {
-      fprintf(stdout, "\n");
-
-      /* get the number of fields */
-      nrows = PQntuples(res);
-      nfields = PQnfields(res);
-
-      /* for each field, get the needed width */
-      length = (int *) pg_malloc(sizeof(int) * nfields);
-      for (j = 0; j < nfields; j++)
-        length[j] = strlen(PQfname(res, j));
-
-      for (i = 0; i < nrows; i++)
+      for (int ligne = 0 ; ligne < PQntuples(res) ; ligne++)
       {
-        for (j = 0; j < nfields; j++)
+        for (int colonne = 0 ; colonne < PQnfields(res) ; colonne++)
         {
-          l = strlen(PQgetvalue(res, i, j));
-          if (l > length[j])
-            length[j] = strlen(PQgetvalue(res, i, j));
+          printf("%s - ", PQgetvalue(res, ligne, colonne));
         }
+        printf("\n");
       }
 
-      /* print a header */
-      for (j = 0, l = 0; j < nfields; j++)
-      {
-        fprintf(stdout, "%*s", length[j] + 2, PQfname(res, j));
-        l += length[j] + 2;
-      }
-      fprintf(stdout, "\n");
-      pad = (char *) pg_malloc(l + 1);
-      memset(pad, '-', l);
-      pad[l] = '\0';
-      fprintf(stdout, "%s\n", pad);
-      free(pad);
-
-      /* for each row, dump the information */
-      for (i = 0; i < nrows; i++)
-      {
-        for (j = 0; j < nfields; j++)
-          fprintf(stdout, "%*s", length[j] + 2, PQgetvalue(res, i, j));
-        fprintf(stdout, "\n");
-      }
-
-      /* cleanup */
       PQclear(res);
-      free(length);
     }
 
-    PQclear(res);
+    printf("\n");
 
     sleep(1);
   }
